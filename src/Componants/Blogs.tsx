ce9098c41dce73b92ddf220f79901cc97c9261df
Ouali Mohammed <oualimhmd06@gmail.com>
2023-10-28 15:15:24 +0100

    change path of img

--- src/Componants/Blogs.tsx
+++ src/Componants/Blogs.tsx
@@ -1,27 +1,27 @@
 export const Blogs = [
   {
     slug: "figuig",
-    img: "./src/assets/img/figuig.png",
+    img: "src\\assets\\img\\figuig.png",
     url: "https://conseilprovincialfiguig.ma/",
   },
   {
-    img: "./src/assets/img/asmgaz.png",
+    img: "src\\assets\\img\\asmgaz.png",
     url: "http://asmgaz.fcpo.agency/index.php",
   },
   {
-    img: "./src/assets/img/acua.png",
+    img: "src\\assets\\img\\acua.png",
     url: "http://acquaclima.fcpo.agency/index.php",
   },
   {
-    img: "./src/assets/img/valinco.png",
+    img: "src\\assets\\img\\valinco.png",
     url: "http://valinco-froid.fcpo.agency/",
   },
   {
-    img: "./src/assets/img/pneu.png",
+    img: "src\\assets\\img\\pneu.png",
     url: "https://pneuscom9.fcpo.agency/",
   },
   {
-    img: "./src/assets/img/learning_land.png",
+    img: "src\\assets\\img\\learning_land.png",
     url: "#",
   },
 ];
