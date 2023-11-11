f7f33bc3c37d3c5b12fdf0c115820760db1fd0eb
Ouali Mohammed <oualimhmd06@gmail.com>
2023-11-11 17:35:24 +0100

    change all img url

--- src/Componants/Blogs.tsx
+++ src/Componants/Blogs.tsx
@@ -5,23 +5,23 @@ export const Blogs = [
     url: "https://conseilprovincialfiguig.ma/",
   },
   {
-    img: "src\\assets\\img\\asmgaz.png",
+    img: "/assets/img/asmgaz.png",
     url: "http://asmgaz.fcpo.agency/index.php",
   },
   {
-    img: "src\\assets\\img\\acua.png",
+    img: "/assets/img/acua.png",
     url: "http://acquaclima.fcpo.agency/index.php",
   },
   {
-    img: "src\\assets\\img\\valinco.png",
+    img: "/assets/img/valinco.png",
     url: "http://valinco-froid.fcpo.agency/",
   },
   {
-    img: "src\\assets\\img\\pneu.png",
+    img: "/assets/img/pneu.png",
     url: "https://pneuscom9.fcpo.agency/",
   },
   {
-    img: "src\\assets\\img\\learning_land.png",
+    img: "/assets/img/learning_land.png",
     url: "#",
   },
 ];
